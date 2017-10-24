55b32f63a2f3fc424623b83a0222ef5653fc3d77
Jerry Hsiung <shsiung@g.hmc.edu>
2017-10-24 09:24:56 -0400

    adding old codes

---
+++ skewsym3.m
@@ -0,0 +1,8 @@
+function [ X ] = skewsym3( x )
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+X = [0 -x(3) x(2) ; 
+       x(3) 0 -x(1) ; 
+       -x(2) x(1) 0 ];
+end
+
