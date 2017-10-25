b43c398b2a8a8b168ecea4d9cd35d89ad65b023f
shsiung <shsiung@g.hmc.edu>
2017-10-24 22:49:23 -0400

    delete files

--- skewsym3.m
+++ skewsym3.m
@@ -1,6 +1,5 @@
 function [ X ] = skewsym3( x )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+% Calculate skew symmetric matrix
 X = [0 -x(3) x(2) ; 
        x(3) 0 -x(1) ; 
        -x(2) x(1) 0 ];
